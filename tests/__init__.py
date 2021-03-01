9f55f65c857b55ba0e44de08d509ffc7d2b85745
Erik Cederstrand <erik@cederstrand.dk>
2021-03-01 15:14:11 +0100

    Randomize order of running test suites and test cases - useful for smoking out accidental test dependencies

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,23 @@
 import logging
+import random
 import sys
-import unittest
+from unittest import TestLoader, TestSuite
+import unittest.util
 
 from exchangelib.util import PrettyXmlHandler
+
+
+class RandomTestSuite(TestSuite):
+    def __iter__(self):
+        tests = list(super().__iter__())
+        random.shuffle(tests)
+        return iter(tests)
+
+
+# Execute test classes in random order
+TestLoader.suiteClass = RandomTestSuite
+# Execute test methods in random order within each test class
+TestLoader.sortTestMethodsUsing = lambda _, x, y: random.choice((1, -1))
 
 # Always show full repr() output for object instances in unittest error messages
 unittest.util._MAX_LENGTH = 2000
